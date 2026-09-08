b1230b60292517774a840aa5de297b4ea2997a4d
Dhruvil Desai <dhruvil.desai@adit.com>
2026-09-08 10:51:17 +0530

    chore!: remove retired TestNG library, superseded by chatnotify v2

--- src/main/java/com/teamninja/utilities/BaseTest.java
+++
@@ -1,9 +0,0 @@
-package com.teamninja.utilities;
-
-import org.testng.annotations.Listeners;
-
-@Listeners(com.teamninja.utilities.SuiteListener.class)
-public class BaseTest {
-    // Extend this class in your test classes to enable Google Chat notifications
-    // when running individual test files.
-}
